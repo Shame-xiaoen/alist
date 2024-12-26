7a0fde28e7762d52590fc8eb4dc9b9c4321ea18c
Scoutttus <15314269467@163.com>
2024-12-26 12:37:21 +0800

    Update runtime.js

--- runtime.js
+++ runtime.js
@@ -1,5 +1,5 @@
 function updateRuntime() {
-    const startDate = new Date('2024-12-22T00:00:00'); // 建站日期
+    const startDate = new Date('2024-12-26T00:00:00'); // 建站日期
     const now = new Date();
     const elapsed = now - startDate;
 
